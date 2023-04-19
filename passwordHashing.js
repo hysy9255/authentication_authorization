65f310e22dcda444395efc5237dc0f6598ed030b
HyungJoon Yoon <hysy9255@gmail.com>
2023-04-19 12:59:48 +0900

    refactoring

--- passwordHashing.js
+++ passwordHashing.js
@@ -1,27 +1,5 @@
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const hashPassword = async (password) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log(hashedPassword);
 };
-
-hashPassword("adminPassword");
-
-// const myFunction = async () => {
-//   const hashedUserName = await bcrypt.hash("윤형준", 10);
-//   const payload = {
-//     userName: hashedUserName,
-//     isAdmin: "hi",
-//   };
-
-//   const token = jwt.sign(payload, "hello");
-//   console.log("token : ");
-//   console.log(token);
-//   // import jwt_decode from "jwt-decode";
-//   const jwt_decode = require("jwt-decode");
-//   const decoded_jwt = jwt_decode(token);
-//   console.log("decoded token : ");
-//   console.log(decoded_jwt);
-// };
-
-// myFunction();
